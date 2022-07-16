06af72171e765c6124bb5e11218c0fcd26f3260e
Dan Yates <dyates750@gmail.com>
2022-07-16 23:46:17 +0100

    Arm swipe script

---
+++ Assets/Scripts/Arm/Arm.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arm : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        // Note: this doesn't quite work when a prop is pushed by another prop,
+        //       but works when the arm hits props directly.
+        var prop = other.gameObject.GetComponent<Prop>();
+        if (prop)
+        {
+            prop.points = 0;
+        }
+    }
+}
