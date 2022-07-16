8c63325f5a4552eb44c1b8390282fbdde3331a32
Dan Yates <dyates750@gmail.com>
2022-07-16 23:26:33 +0100

    Add arm swipe

--- Assets/Scripts/Enemies/Faller/FallerManager.cs
+++ Assets/Scripts/Enemies/Faller/FallerManager.cs
@@ -6,14 +6,26 @@ using Random = UnityEngine.Random;
 
 public class FallerManager : MonoBehaviour
 {
-    [SerializeField] private GameObject _faller;
+    [SerializeField] private List<GameObject> _fallers;
 
     private BoxCollider _boxCollider;
 
     private void Awake()
     {
         _boxCollider = GetComponent<BoxCollider>();
-        Instantiate(_faller, RandomPositionInBounds(_boxCollider.bounds), Quaternion.identity);
+    }
+
+    private void Start()
+    {
+        // Repeatedly spawn enemies
+        InvokeRepeating(nameof(SpawnRandomFallerInRandomPosition), 0f, 0.5f);
+        // CancelInvoke(nameof(SpawnRandomFallerInRandomPosition));
+    }
+
+    private void SpawnRandomFallerInRandomPosition()
+    {
+        var i = Random.Range(0, _fallers.Count);
+        Instantiate(_fallers[i], RandomPositionInBounds(_boxCollider.bounds), Quaternion.identity);
     }
 
     private Vector3 RandomPositionInBounds(Bounds bounds)
