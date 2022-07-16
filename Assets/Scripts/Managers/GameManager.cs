81be9bf82d23cb82021c5aae5cfe041a4cb44ff2
Dan Yates <dyates750@gmail.com>
2022-07-16 11:25:48 +0100

    Add a cute font and do some other stuff

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -27,6 +28,7 @@ public class GameManager : MonoBehaviour
 
     public void RestartGame()
     {
-        Debug.Log("Hello :)");
+        _UIManager.ShowGameOverScreen(false);  // Hide game over UI
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);  // Restart current level
     }
 }
